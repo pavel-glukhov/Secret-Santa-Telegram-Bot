00960548d36558d041676b476652d8be9c0bed14
pavel-glukhov <glukhov.p@gmail.com>
2022-07-16 01:06:37 +0600

    Updated get_user and create_user requests

--- app/database/operations.py
+++ app/database/operations.py
@@ -3,7 +3,7 @@ from typing import ClassVar
 
 from sqlalchemy.exc import SQLAlchemyError
 
-from app.database.models import User, Room, RoomUser
+from app.database.models import User, Room
 from sqlalchemy import select
 import logging
 
@@ -12,15 +12,23 @@ class UserDB:
     def __init__(self, session):
         self.db_session = session
 
-    async def create_user(self, username: str, telegram_id: int, **kwargs):
-        self.db_session.add(User(username=username, telegram_id=telegram_id, **kwargs))
+    async def create_user(self,
+                          username: str,
+                          user_id: int,
+                          **kwargs) -> None:
+
+        self.db_session.add(User(username=username,
+                                 telegram_id=user_id,
+                                 **kwargs))
         try:
             await self.db_session.commit()
         except SQLAlchemyError as ex:
             logging.error(f"UserDB: 'crete user' method: {str(ex)}")
 
-    async def get_user(self, telegram_id: int) -> ClassVar[User]:
-        user = await self.db_session.execute(select(User).where(telegram_id == telegram_id))
+    async def get_user(self, user_id: int) -> User:
+        user = await self.db_session.execute(
+            select(User).where(User.telegram_id == user_id)
+        )
         return user.scalars().one_or_none()
 
     async def update_user(self):
@@ -37,9 +45,16 @@ class RoomDB:
     def __init__(self, session):
         self.db_session = session
 
-    async def create_room(self, name: str, number: int, owner_id: int, budget: str):
-        user = await UserDB(self.db_session).get_user(telegram_id=owner_id)
-        room = Room(name=name, number=number, owner=user, budget=budget)
+    async def create_room(self, name: str,
+                          number: int,
+                          owner_id: int,
+                          budget: str) -> None:
+        user = await UserDB(self.db_session).get_user(user_id=owner_id)
+        room = Room(name=name,
+                    number=number,
+                    owner=user,
+                    budget=budget)
+
         room.members = [user]
         self.db_session.add(room)
 
@@ -55,6 +70,9 @@ class RoomDB:
         pass
 
     async def remove_member(self):
+        pass
+
+    async def get_all_assigned_rooms(self, user_id):
         pass
 
     async def delete_room(self):
