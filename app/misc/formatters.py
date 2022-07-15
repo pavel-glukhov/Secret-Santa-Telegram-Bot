883e14b9252e915aa365953ab746bb5f9d238b6c
pavel-glukhov <glukhov.p@gmail.com>
2022-07-16 05:02:03 +0600

    fixed typing

--- app/misc/formatters.py
+++ app/misc/formatters.py
@@ -1,7 +1,7 @@
 from app.database.models import User
 
 
-async def user_information_formatter(user: User):
+async def user_information_formatter(user: User) -> str:
     first_name = user.first_name or ''
     last_name = user.last_name or ''
     address = user.address or 'адрес не указан'
