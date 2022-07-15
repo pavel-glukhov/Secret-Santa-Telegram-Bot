5aa127d2faf89e00bcd15c50c5a2bfd1b99b111d
pavel-glukhov <glukhov.p@gmail.com>
2022-07-16 05:01:32 +0600

    Added memory storage for states

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, Dispatcher, types
 import logging
 
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 from app.settings import config
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot.token)
-dispatcher = Dispatcher(bot)
+storage = MemoryStorage()
+dispatcher = Dispatcher(bot, storage=storage)
 
 from app.handlers import *
