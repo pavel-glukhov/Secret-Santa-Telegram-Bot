74ce56462be750090b3ad5f422a83440d97e21f9
pavel-glukhov <glukhov.p@gmail.com>
2022-07-16 01:04:30 +0600

    Add callback module: wrote profile callback functions

---
+++ app/handlers/callbacks.py
@@ -0,0 +1,33 @@
+from aiogram.dispatcher.filters import Text
+from aiogram import types
+
+from app import dispatcher as dp, bot
+from app.handlers.profiles import edit_profile
+
+
+@dp.callback_query_handler(Text(startswith="profile_edit"))
+async def edit_user_profile(callback: types.CallbackQuery):
+    if callback.data == 'profile_edit':
+        await edit_profile(callback.message)
+    if callback.data == 'profile_edit_name':
+        pass
+    if callback.data == 'profile_edit_address':
+        pass
+    if callback.data == 'profile_edit_number':
+        pass
+    if callback.data == 'profile_edit_delete_all':
+        pass
+
+
+@dp.message_handler(lambda message: message.text == "Об игре ℹ️")
+async def about_game(message: types.Message):
+    msg = 'Это адаптированная игра "Тайный Санта"'
+    await message.answer(msg)
+
+
+# TODO потом переделать под callback
+@dp.callback_query_handler(text="about_game")
+async def callbacks_num(call: types.CallbackQuery):
+    if call.data == "about_game":
+        await bot.answer_callback_query(call.id)
+        await bot.send_message(call.from_user.id, 'Нажата кнопка об игре!')
