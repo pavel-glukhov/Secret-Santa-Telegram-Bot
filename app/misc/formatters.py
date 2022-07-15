2e19e12d73e40d87b7849df3f5c1f2f0978bd3fb
pavel-glukhov <glukhov.p@gmail.com>
2022-07-16 01:38:27 +0600

    Added formatter for user information model

---
+++ app/misc/formatters.py
@@ -0,0 +1,16 @@
+from app.database.models import User
+
+
+async def user_information_formatter(user: User):
+    first_name = user.first_name or ''
+    last_name = user.last_name or ''
+    address = user.address or 'адрес не указан'
+    number = user.contact_number or 'номер не указан'
+    email = user.email or 'email не указан'
+
+    formatted_text = (f"*Полное имя*: {first_name} {last_name}\n"
+                      f"*Адрес*: {address}\n"
+                      f"*Номер телефона*: {number}\n"
+                      f"*Email*: {email}")
+
+    return formatted_text
