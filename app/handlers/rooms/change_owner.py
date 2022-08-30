e3c30917bdaa9c75dc29619391b97c1eaf53a0bb
pavel-glukhov <glukhov.p@gmail.com>
2022-08-30 14:15:44 +0600

    Added change owner functions

--- app/handlers/rooms/change_owner.py
+++ app/handlers/rooms/change_owner.py
@@ -5,8 +5,8 @@ from aiogram.types import ParseMode
 
 from app import dispatcher as dp
 from app.database import room_db
+from app.database.models import User
 from app.keyborads.common import create_common_keyboards
-from app.keyborads.constants import MAIN_REPLY_BUTTONS
 
 
 class ChangeOwner(StatesGroup):
@@ -14,4 +14,36 @@ class ChangeOwner(StatesGroup):
 
 
 async def change_room_owner(message: types.Message, room_number):
-    pass
+    await ChangeOwner.waiting_for_owner_name.set()
+    state = dp.get_current().current_state()
+    await state.update_data(room_number=room_number)
+    await message.answer(
+        'Хочешь поменять владельца комнаты?\n'
+        'Новый владелец комнаты должен являться ее участником. '
+        '*Учти, что ты потеряешь контроль за комнатой.*\n\n'
+        '*Для смены владельца, напиши его ник.*\n\n'
+        'Что бы отменить процесс, введите в чате *отмена*',
+        parse_mode=ParseMode.MARKDOWN,
+    )
+
+
+async def process_changing_owner(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        new_owner: User = await room_db().change_owner(message.text,
+                                                       room_number=data[
+                                                           'room_number'])
+        keyboard = await create_common_keyboards(message)
+        await state.finish()
+        if new_owner:
+            await message.answer(
+                '"Хо-хо-хо! 🎅\n\n'
+                f'Я сменил владельца, теперь это *{new_owner.username}*',
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=keyboard
+            )
+        else:
+            await message.answer(
+                'Такой участник не найден.',
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=keyboard
+            )
