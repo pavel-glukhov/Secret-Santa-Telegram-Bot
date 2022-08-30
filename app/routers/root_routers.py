5d563b4b33c6498b30837b2317fa2dc403868747
pavel-glukhov <glukhov.p@gmail.com>
2022-08-30 14:10:39 +0600

    REMOVED cancel_handler handler

--- app/routers/root_routers.py
+++ app/routers/root_routers.py
@@ -6,11 +6,7 @@ from app.keyborads.constants import MAIN_REPLY_BUTTONS
 
 
 def setup_cancel_handlers(dp: Dispatcher):
-    # cancel handlers
-    dp.register_message_handler(
-        cancel_handler, Text(contains=[x for x in MAIN_REPLY_BUTTONS.values()]),
-        state='*'
-    )
+
     dp.register_message_handler(
         cancel_handler,
         state='*', commands='отмена'
