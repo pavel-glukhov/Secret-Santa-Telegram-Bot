b79327d1ac30f32bf5dee4c4d58d216576ad6ead
pavel-glukhov <glukhov.p@gmail.com>
2022-09-04 21:41:07 +0600

    Added notification for users who are not updated their profile information

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -7,27 +7,38 @@ from app.keyborads.common import create_common_keyboards
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
+    user_id = message.chat.id
+    username = message.chat.username
+    first_name = message.chat.first_name
+    last_name = message.chat.last_name
+
+    user, created = await user_db().get_or_create(
+        user_id=user_id,
+        username=username,
+        first_name=first_name,
+        last_name=last_name
+    )
+
     await message.answer(
         "Хо-хо-хо! 🎅\n\n"
         "Вот и настало поиграть в Тайного Санту!\n\n"
         "Создай свою комнату для друзей, или подключись к существующей.",
     )
+
+    if not any([user.address, user.contact_number]):
+        await message.answer(
+            'Не забудь обновить свои контактные данные в настройках профиля,'
+            'что бы твой Тайный Санта смог отправить тебе подарок!!\n\n'
+
+        )
     await root_menu(message)
 
 
 @dp.message_handler(commands=['menu'])
 async def root_menu(message: types.Message, edit_previous_message=False):
     user_id = message.chat.id
-    username = message.chat.username
-    first_name = message.chat.first_name
-    last_name = message.chat.last_name
-
-    await user_db().create_user_or_get(username=username,
-                                       user_id=user_id,
-                                       first_name=first_name,
-                                       last_name=last_name)
-
     keyboard = await create_common_keyboards(message)
+
     await room_db().get_all_user_rooms(user_id)
     if edit_previous_message:
         await message.edit_text("Меню", reply_markup=keyboard)
