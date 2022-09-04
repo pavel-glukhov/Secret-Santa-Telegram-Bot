ba893de6a5f16a6c9542d146ab3cb89b9779c8b3
pavel-glukhov <glukhov.p@gmail.com>
2022-09-04 21:39:52 +0600

    Updated text for validators

--- app/handlers/rooms/validators.py
+++ app/handlers/rooms/validators.py
@@ -1,10 +1,12 @@
 from aiogram import types
 from aiogram.types import ParseMode
 from app import dispatcher as dp
+from app.handlers.profiles.profile_change_number import ChangePhoneNuber
 from app.handlers.rooms.create_room import CreateRoom
 from app.handlers.rooms.subscribe_room import JoinRoom
 from app.handlers.rooms.update_room import ChangeRoomName
 from app.keyborads.common import keyboard_button
+import re
 
 
 @dp.message_handler(lambda message: len(message.text) > 12,
@@ -30,8 +32,9 @@ async def process_join_room_invalid_text_type(message: types.Message):
                                       callback='cancel')
     return await message.reply(
         text='Номер комнаты может содержать только цифры, '
-             'попробуйте снова.\n'
-             'Что бы отменить процесс, введите в чате *отмена*',
+             'попробуйте снова.\n',
         parse_mode=ParseMode.MARKDOWN,
         reply_markup=keyboard_inline
     )
+
+
