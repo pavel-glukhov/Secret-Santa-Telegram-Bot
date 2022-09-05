316a43ab33894c1ec4f5e831cbc87fdb74bea58f
pavel-glukhov <glukhov.p@gmail.com>
2022-09-05 16:25:16 +0600

    updated imports

--- app/handlers/rooms/__init__.py
+++ app/handlers/rooms/__init__.py
@@ -1,2 +1,13 @@
-from app.handlers.rooms import (common, create_room, delete_room,
-                                subscribe_room, unsubscribe_room, update_room)
+from app.handlers.rooms import (
+    validators,
+    change_owner,
+    common,
+    config_room,
+    create_room,
+    delete_room,
+    start_game_room,
+    subscribe_room,
+    unsubscribe_room,
+    update_room,
+
+)
