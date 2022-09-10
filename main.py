6e5afc8966de0eff1ba6a3e9e5b56a877cd515c5
pavel-glukhov <glukhov.p@gmail.com>
2022-09-10 17:13:29 +0600

    Added logging

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
 import asyncio
-import logging
-
+import logging.config
+import os
+import yaml
+from app.config import root_path
 from app import bot, config, dispatcher
 from app.database import database_initialization
-from app.logger import create_log_directory
 from app.scheduler import scheduler
 
 logger = logging.getLogger(__name__)
 
+
+def setup_logging() -> None:
+    configuration_file = os.path.join(root_path, config.log.config_file)
+
+    with open(configuration_file, 'r') as stream:
+        logging_config = yaml.load(stream, Loader=yaml.FullLoader)
+
+    # Create log directories if not exists
+    if not os.path.exists(config.log.log_path):
+        os.makedirs(config.log.log_path)
+
+    logging_config['handlers']['timed_rotating_handler'][
+        'filename'] = os.path.join(config.log.log_path, config.log.log_file)
+
+    logging.config.dictConfig(logging_config)
+
+
 async def main():
-    logger.info('-------------------')
-    logger.info('Bot is started')
-    logger.info('-------------------')
+    logger.info('Bot starting...')
     await database_initialization()
     await dispatcher.start_polling(bot)
 
 
 if __name__ == "__main__":
-    create_log_directory(config.log.log_path)
+    setup_logging()
     scheduler.start()
     try:
         asyncio.run(main())
+
     except (KeyboardInterrupt, SystemExit):
         logger.info('-------------------')
         logger.info('Bot has been stopped')
