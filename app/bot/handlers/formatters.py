3f27cf13fdcea35eaa285dc58b952953f8ec3de5
Pavel Glukhov <glukhov.p@gmail.com>
2022-12-30 18:28:26 +0600

    Updated code style

--- app/bot/handlers/formatters.py
+++ app/bot/handlers/formatters.py
@@ -2,9 +2,13 @@ from app.store.database.models import User
 
 
 def user_information_formatter(user: User) -> list:
-    f_n = user.first_name
-    l_n = user.last_name
-    full_name = f'{f_n} {l_n}' if any([f_n, l_n]) else 'Имя не указано'
+    first_name = user.first_name
+    last_name = user.last_name
+    full_name = (
+        f'{first_name} {last_name}'
+        if any([first_name, last_name])
+        else 'Имя не указано'
+    )
     address = user.address or 'адрес не указан'
     number = user.contact_number or 'номер не указан'
     return [full_name, address, number]
@@ -12,8 +16,9 @@ def user_information_formatter(user: User) -> list:
 
 def profile_information_formatter(user: User) -> str:
     full_name, address, number = user_information_formatter(user)
-    formatted_text = (f"<b>Полное имя</b>: {full_name}\n"
-                      f"<b>Адрес</b>: {address}\n"
-                      f"<b>Номер телефона</b>: {number}\n"
-                      )
+    formatted_text = (
+        f"<b>Полное имя</b>: {full_name}\n"
+        f"<b>Адрес</b>: {address}\n"
+        f"<b>Номер телефона</b>: {number}\n"
+    )
     return formatted_text
