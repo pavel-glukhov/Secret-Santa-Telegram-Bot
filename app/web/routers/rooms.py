34c3e1f1f82c4a1cc1cfac0a4b3f387d9179c25b
Pavel Glukhov <glukhov.p@gmail.com>
2023-06-24 04:15:15 +0600

    Created html templates

---
+++ app/web/routers/rooms.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, Request
+from starlette.responses import HTMLResponse
+from app.store.database.queries.rooms import RoomDB
+from app.web.dependencies import get_current_user
+from app.web.exceptions.app_exceptions import (
+    NotAccessException,
+    NotAuthenticatedException
+)
+from app.config import templates
+
+router = APIRouter()
+
+
+@router.get("/rooms", name='rooms')
+async def index(request: Request, current_user=Depends(get_current_user)):
+    if not current_user:
+        raise NotAuthenticatedException(status_code=401)
+    
+    context = {
+        'request': request,
+        'id': current_user,
+        'username': request.cookies.get('username'),
+        'first_name': request.cookies.get('first_name'),
+        'last_name': request.cookies.get('last_name'),
+    }
+    
+    return templates.TemplateResponse(
+        'rooms.html', context=context)
+
+
+@router.get("/active_games", name='active_games')
+async def index(request: Request, current_user=Depends(get_current_user)):
+    if not current_user:
+        raise NotAuthenticatedException(status_code=401)
+    
+    context = {
+        'request': request,
+        'id': current_user,
+        'username': request.cookies.get('username'),
+        'first_name': request.cookies.get('first_name'),
+        'last_name': request.cookies.get('last_name'),
+    }
+    
+    return templates.TemplateResponse(
+        'active_games.html', context=context)
