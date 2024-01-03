5a206669dcb76775045cb36b0e583f8d8de06d24
Pavel Glukhov <glukhov.p@gmail.com>
2024-01-03 23:11:16 +0600

    Refactoring

--- app/store/database/__init__.py
+++ app/store/database/__init__.py
@@ -24,6 +24,7 @@ TORTOISE_ORM = {
     },
 }
 
+
 async def init_db() -> None:
     try:
         await Tortoise.init(
@@ -31,6 +32,7 @@ async def init_db() -> None:
         )
     except exceptions.DBConnectionError as ex:
         logger.error(ex)
+
 
 async def close_db() -> None:
     await Tortoise.close_connections()
--- main.py
+++ main.py
@@ -22,11 +22,13 @@ exception_handlers = {
     500: app_exceptions.internal_error,
 }
 
+
 @AuthJWT.load_config
 def get_config():
     return AuthJWTSettings()
 
-def init_routers(app: FastAPI)-> None:
+
+def init_routers(app: FastAPI) -> None:
     app.include_router(main.router)
     app.include_router(users.router)
     app.include_router(auth.router)
@@ -34,10 +36,12 @@ def init_routers(app: FastAPI)-> None:
     app.include_router(active_games.router)
     app.include_router(webhooks.router)
 
+
 def init_handlers(app: FastAPI) -> None:
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
-    
+
+
 def create_app() -> FastAPI:
     app = FastAPI(
         debug=False,
@@ -54,20 +58,20 @@ def create_app() -> FastAPI:
     init_routers(app)
     return app
 
+
 async def on_startup():
     setup_logging()
     await init_db()
     scheduler.start()
     webhook_url = webhook_settings(load_config).get('webhook_url')
     webhook_info = await bot.get_webhook_info()
-
+    
     if webhook_info.url != webhook_url:
         await bot.set_webhook(url=webhook_url)
     logger.info("App has been started")
+
 
 async def on_shutdown():
     await bot.session.close()
     await close_db()
     logger.info("App has been stopped")
-
-
