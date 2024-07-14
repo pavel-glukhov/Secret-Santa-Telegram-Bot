8427f1be2f9de95ab2a3f43b3459d44a0ff6dae1
Pavel Glukhov <glukhov.p@gmail.com>
2024-07-14 21:28:12 +0500

    removed all imports in init

--- app/bot/handlers/rooms/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.bot.handlers.rooms import (activate, change_budget, change_owner,
-                                    config_room, create_new_room, delete_room,
-                                    main, members, subscribe, unsubscribe,
-                                    update_room)
