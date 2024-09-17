326729c61d51b8180efae4abbeccd8999a35eeee
Pavel Glukhov <glukhov.p@gmail.com>
2024-09-17 10:45:28 +0500

    Added redis pool

--- app/store/redis.py
+++ app/store/redis.py
@@ -1,12 +1,21 @@
-from redis import Redis
+from redis import Redis, ConnectionPool
 from app.config import load_config
 
 
-def get_redis_client():
+def create_redis_pool():
     config = load_config()
-    return Redis(host=config.redis.host,
-                 port=int(config.redis.port),
-                 db=config.redis.db,
-                 password=config.redis.password,
-                 decode_responses=True,
-                 encoding="utf-8")
+    return ConnectionPool(
+        host=config.redis.host,
+        port=int(config.redis.port),
+        db=config.redis.db,
+        password=config.redis.password,
+        decode_responses=True,
+        encoding="utf-8"
+    )
+
+
+pool = create_redis_pool()
+
+
+def get_redis_client():
+    return Redis(connection_pool=pool)
