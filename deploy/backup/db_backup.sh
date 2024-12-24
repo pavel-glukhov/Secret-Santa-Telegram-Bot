250b4cc31ef5b598769154dbd9d3f4d2e09595e1
Pavel glukhov <glukhov.p@gmail.com>
2024-12-24 14:28:00 +0500

    Updated

--- deploy/backup/db_backup.sh
+++ deploy/backup/db_backup.sh
@@ -6,11 +6,12 @@
 BACKUP_DIR=/backups/database/
 DAYS_TO_KEEP=30
 FILE_SUFFIX=_db_backup.sql
-DATABASE=procharrity
+DATABASE=NAME_DB
 USER=postgres
+CONTAINER_NAME=your_DB_container_name
 
 FILE=`date +"%Y%m%d%H%M"`${FILE_SUFFIX}
 OUTPUT_FILE=${BACKUP_DIR}/${FILE}
-pg_dump -U ${USER} ${DATABASE} -F p -f ${OUTPUT_FILE}
+docker exec -t ${CONTAINER_NAME} pg_dump -U ${USER} ${DATABASE} -F p -f ${OUTPUT_FILE}
 gzip $OUTPUT_FILE
 find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${FILE_SUFFIX}.gz" -exec rm -rf '{}' ';'
