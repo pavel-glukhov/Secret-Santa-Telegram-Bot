45a71d0b71b70ce0b47bc49618ae6d2bb5c8d9f2
Pavel glukhov <glukhov.p@gmail.com>
2025-01-08 13:57:43 +0500

    Added tests of users db repo

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from app.store.database.models import Base
+
+test_engine = create_engine("sqlite:///:memory:")
+TestSession = scoped_session(sessionmaker(bind=test_engine))
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    Base.metadata.create_all(test_engine)
+    yield
+    Base.metadata.drop_all(test_engine)
+
+@pytest.fixture
+def session():
+    session = TestSession()
+    yield session
+    session.close()
