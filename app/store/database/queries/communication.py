fd55ea5a7d85b93b186990969080316e2b2637c0
Pavel glukhov <glukhov.p@gmail.com>
2025-01-10 16:17:58 +0500

    Refactoring

--- app/store/database/queries/communication.py
+++ app/store/database/queries/communication.py
@@ -2,14 +2,14 @@ from datetime import datetime
 
 from sqlalchemy.orm import Session
 
-from app.store.database.models import UsersMessages
+from app.store.database.models import UsersMessages, Room
 
 
 class CommunicationRepo:
     def __init__(self, session: Session):
         self.session = session
     
-    async def insert(self, room: int, recipient_id: int, sender_id: int, message: str) -> None:
+    async def insert(self, room: Room, recipient_id: int, sender_id: int, message: str) -> None:
         new_message = UsersMessages(
             recipient_id=recipient_id,
             sender_id=sender_id,
