dd81ac47bb62112c824b7cc9e803b7202d24b2be
Pavel glukhov <glukhov.p@gmail.com>
2025-05-05 23:18:37 +0500

    MODIFIED: changed interface

--- app/bot/handlers/profiles/delete_information.py
+++ app/bot/handlers/profiles/delete_information.py
@@ -19,11 +19,14 @@ router = Router()
 async def delete_user_information(callback: types.CallbackQuery,
                                   state: FSMContext,
                                   lang: TranslationMainSchema):
+    confirm_button = lang.buttons.confirm_button
     cancel_button = lang.buttons.cancel_button
     keyboard_inline = generate_inline_keyboard(
-        {cancel_button: 'cancel'}
+        {
+            confirm_button: 'confirm_deletion',
+            cancel_button: 'cancel_deletion'
+        }
     )
-
     message_text = lang.messages.profile_menu.delete_information.delete_information_first_msg
 
     initial_bot_message = await callback.message.edit_text(text=message_text,
@@ -33,36 +36,15 @@ async def delete_user_information(callback: types.CallbackQuery,
     await state.set_state(DeleteUserInformation.waiting_for_conformation)
 
 
-@router.message(lambda message: message.text.lower() not in ['confirm'],
-                StateFilter(DeleteUserInformation.waiting_for_conformation))
-async def process_deleting_information_invalid(message: types.Message,
-                                               state: FSMContext,
-                                               lang: TranslationMainSchema):
-    state_data = await state.get_data()
-    await message.delete()
-
-    bot_message = state_data['bot_message_id']
-
-    cancel_button = lang.buttons.cancel_button
-    keyboard_inline = generate_inline_keyboard(
-        {cancel_button: 'cancel'}
-    )
-
-    message_text = lang.messages.profile_menu.delete_information.error
-
-    return await bot_message.edit_text(text=message_text, reply_markup=keyboard_inline)
-
-
-@router.message(lambda message: message.text.lower() in ['confirm'],
-                StateFilter(DeleteUserInformation.waiting_for_conformation))
-async def process_deleting_information(message: types.Message,
-                                       state: FSMContext,
-                                       session: Session,
-                                       lang: TranslationMainSchema):
-    user_id = message.chat.id
+@router.callback_query(F.data == 'confirm_deletion',
+                      StateFilter(DeleteUserInformation.waiting_for_conformation))
+async def process_deleting_information(callback: types.CallbackQuery,
+                                      state: FSMContext,
+                                      session: Session,
+                                      lang: TranslationMainSchema):
+    user_id = callback.message.chat.id
     state_data = await state.get_data()
 
-    await message.delete()
     bot_message = state_data['bot_message_id']
     data = {
         'first_name': None,
@@ -81,6 +63,26 @@ async def process_deleting_information(message: types.Message,
         }
     )
     message_text = lang.messages.profile_menu.delete_information.delete_information_second_msg
+
+    await bot_message.edit_text(text=message_text, reply_markup=keyboard_inline)
+    await state.clear()
+
+
+@router.callback_query(F.data == 'cancel_deletion',
+                      StateFilter(DeleteUserInformation.waiting_for_conformation))
+async def cancel_deleting_information(callback: types.CallbackQuery,
+                                     state: FSMContext,
+                                     lang: TranslationMainSchema):
+    state_data = await state.get_data()
+    bot_message = state_data['bot_message_id']
+
+    return_back_button = lang.buttons.return_back_button
+    keyboard_inline = generate_inline_keyboard(
+        {
+            return_back_button: "menu_user_profile",
+        }
+    )
+    message_text = lang.messages.profile_menu.delete_information.cancel_message
 
     await bot_message.edit_text(text=message_text, reply_markup=keyboard_inline)
     await state.clear()
