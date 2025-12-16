e0b830a69a9e5426d27becdd74d3497209f21b59
Pavel Glukhov <glukhov.p@gmail.com>
2025-12-16 13:37:26 +0500

    Added additional check in RoomNumberMiddleware

--- app/bot/middlewares/room_number_middleware.py
+++ app/bot/middlewares/room_number_middleware.py
@@ -13,11 +13,15 @@ class RoomNumberMiddleware(BaseMiddleware):
         event: CallbackQuery,
         data: Dict[str, Any]
     ) -> Any:
-        try:
-            room_number = int(event.data[event.data.rfind('_') + 1:])
-            data["room_number"] = room_number
-        except (ValueError, AttributeError):
-            logger.warning("Failed to parse room number from callback data: %s", event.data)
-            data["room_number"] = None
+        data["room_number"] = None
+
+        if event.data and '_' in event.data:
+            potential_number = event.data.rsplit('_', 1)[-1]
+            try:
+                data["room_number"] = int(potential_number)
+            except ValueError:
+                logger.warning(
+                    "Failed to parse room number from callback data: %s", event.data
+                )
 
         return await handler(event, data)
